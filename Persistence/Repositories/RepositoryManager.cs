1112d5a9364edb8d2b7c8ba43c7bcf4334939a35
Ron Le <ronle9519@gmail.com>
2023-06-03 21:55:31 +0700

    feat: init project

---
+++ Persistence/Repositories/RepositoryManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Domain.Repositories;
+
+namespace Persistence.Repositories
+{
+    public sealed class RepositoryManager : IRepositoryManager
+    {
+        private readonly Lazy<IUnitOfWork> _lazyUnitOfWork;
+
+        public RepositoryManager(RepositoryDbContext dbContext)
+        {
+            _lazyUnitOfWork = new Lazy<IUnitOfWork>(() => new UnitOfWork(dbContext));
+        }
+        public IUnitOfWork UnitOfWork => _lazyUnitOfWork.Value;
+    }
+}
---
+++ Persistence/RepositoryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public sealed class RepositoryDbContext : DbContext
+    {
+        public RepositoryDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(RepositoryDbContext).Assembly);
+    }
+}
